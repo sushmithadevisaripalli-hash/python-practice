0e7d0afd6a87af3197aee5b012f2e54cd0bfbd3c
Sushmitha Devi Saripalli <sushmithadevisaripalli@gmail.com>
2026-06-11 19:10:46 -0400

    Add Day 1 and Day 2 practice files

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+print("My name is Sushmitha and I'll be an AI/ML engineer in the future!")
+
+name = "Sushmitha"
+age = 24
+city = "Toledo"
+print("My name is " + name + ", I am " + str(age) + " years old, and I live in "+ city + ".")
+
+a= 10
+b= 3
+print("The sum of a and b is: " + str(a+b))
+print("The difference of a and b is: " + str(a-b))
+print("The product of a and b is: " + str(a*b))
+print("The quotient of a and b is: " + str(a/b))
+
